598efc76372573706b45427fdfaf7ed079b67ea1
Farfadeli <ndaunac@gmail.com>
2025-04-30 14:43:02 +0200

    add function percent

--- src/percent.py
+++ src/percent.py
@@ -4,43 +4,105 @@ import geopandas as gpd
 import os
 import argparse
 import glob
+import json
+
 
 def cleanse(folder):
     files = glob.glob(folder + "*")
     for file in files:
         os.remove(file)
 
-def raster_cutter(raster, shapefile):
+
+def value_to_code(stats : dict, json_file, index: int) -> dict:
+    result = {}
+    for e in json_file["configurations"][index]["data"]["legend"]["details"] :
+        if str(int(e["value"])) in stats:
+            result[e["code"]] = stats[str(int(e["value"]))]
+    return result
+    
+
+def calculate_stats(src, json_file, index) -> dict:
+    data = src.read(1)
+    height, width = data.shape
+    stats = {}
+    for row in range(height):
+        for col in range(width):
+            value = data[row, col]
+            if str(value) in stats:
+                stats[str(value)] += 1
+            else :
+                stats[str(value)] = 1
+    return(value_to_code(stats, json_file, index))
+
+def cutter(raster, shapefile: str, savefile: str = "", json_file: str = "") -> list:
     tmp_file = "tmp_"
     output_dir = "../tmp"
-    if type(raster) == str:
-        src = rasterio.open(raster)
-        gdf = gpd.read_file(shapefile)
+    categorie_code = ""
+    title = ""
+    index = 0
+    out = {}
+    out_with_code = {}
+    
+    raster_name = raster.split("\\")[-1]
+    for e in range(len(json_file["configurations"])) :
+        if "chromaColors" in json_file["configurations"][e]["data"]["legend"] :
+            return []
+        if "path" in json_file['configurations'][e]["data"]:
+            if json_file['configurations'][e]["data"]["path"].split("/")[-1] == raster_name:
+                categorie_code = json_file['configurations'][e]["code"]
+                title = json_file['configurations'][e]["name"]
+                index = e
+                break
 
-        gdf = gdf.to_crs(src.crs)
+    src = rasterio.open(raster)
+    gdf = gpd.read_file(shapefile)
 
-        os.makedirs(output_dir, exist_ok=True)
+    gdf = gdf.to_crs(src.crs)
 
-        for idx, row in gdf.iterrows():
-            geometries = [row['geometry']]
-            code = row['CODE']
-            out_image, out_transform = mask(src, geometries, crop=True)
+    os.makedirs(output_dir, exist_ok=True)
 
-            out_meta = src.meta.copy()
-            out_meta.update({
-                "driver": "GTiff",
-                "height": out_image.shape[1],
-                "width": out_image.shape[2],
-                "transform": out_transform
-            })
-            
-            output = os.path.join(output_dir, f"{tmp_file}{code}.tif")
-            with rasterio.open(output, "w" , **out_meta) as dest:
-                dest.write(out_image)
-                print(f"Raster créer avec succès et enregistré dans : {output_dir}/{tmp_file}{code}.tif ✔️")
+    for idx, row in gdf.iterrows():
+        geometries = [row['geometry']]
+        code = row['CODE']
+        out_image, out_transform = mask(src, geometries, crop=True)
 
-    if type(raster) == list:
-        print(raster)
+        out_meta = src.meta.copy()
+        out_meta.update({
+            "driver": "GTiff",
+            "height": out_image.shape[1],
+            "width": out_image.shape[2],
+            "transform": out_transform
+        })
+
+        output = os.path.join(output_dir, f"{tmp_file}{code}.tif")
+        with rasterio.open(output, "w", **out_meta) as dest:
+            dest.write(out_image)
+            print(
+                f"Raster créer avec succès et enregistré dans : {output_dir}/{tmp_file}{code}.tif ✔️")
+        with rasterio.open(output) as dest:
+            out[code] = calculate_stats(dest, json_file, index)
+    print(out)
+    return out
+
+
+def raster_cutter(raster, shapefile: str, savefile: str = "", json_file: str = ""):
+    
+    json_output = {}
+    
+    with open(json_file, 'r', encoding='utf-8') as file :
+        data = json.load(file)
+        if type(raster) == str:
+            cutter(raster, shapefile, savefile, data)
+        if type(raster) == list:
+            for tif in raster:
+                if len(json_output) == 0 : json_output = cutter(tif, shapefile, savefile, data)
+                else :
+                    out = cutter(tif, shapefile, savefile, data)
+                    
+                    if len(out) == 0 : continue
+                    for key , value in out.items():
+                        json_output[key].append(value)
+                
 
 
 if __name__ == "__main__":
@@ -54,19 +116,24 @@ if __name__ == "__main__":
         "--save_folder", help="Use this flag to set where the result will output")
     parser.add_argument("--tmp", help="define temp folder")
     parser.add_argument("--cleanse", help="Clear all file from a folder")
-    parser.add_argument("--json", help="define json where code is write (Only for DAS)")
+    parser.add_argument(
+        "--json", help="define json where code is write (Only for DAS)")
 
     args = parser.parse_args()
-    
-    if args.shapefile == None and (args.raster != None or args.raster_folder != None) : raise ValueError("You have to define a shapefile to cut raster")
-    
+
+    if args.json == None:
+        raise ValueError(
+            "You have to enter a valid path of json with '--json' and a valid path for the json output with '--save_folder'")
+    if args.shapefile == None and (args.raster != None or args.raster_folder != None):
+        raise ValueError("You have to define a shapefile to cut raster")
+
     raster = args.raster
     if args.raster_folder != None:
         raster = glob.glob(args.raster_folder + "/*.tif")
 
-    if args.cleanse != None :
+    if args.cleanse != None:
         cleanse(args.cleanse)
         print("Tout les fichiers on été supprimer ✔️")
         exit(0)
-    
-    raster_cutter(raster, args.shapefile)
+
+    raster_cutter(raster, args.shapefile, json_file=args.json)
