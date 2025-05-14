3d03381d8e931796ed21ea0d87d9e155017e85f6
Farfadeli <ndaunac@gmail.com>
2025-05-14 10:52:31 +0200

    change percent.py

--- src/percent.py
+++ src/percent.py
@@ -13,28 +13,44 @@ def cleanse(folder):
         os.remove(file)
 
 
-def value_to_code(stats : dict, json_file, index: int) -> dict:
-    result = {}
-    for e in json_file["configurations"][index]["data"]["legend"]["details"] :
-        if str(int(e["value"])) in stats:
-            result[e["code"]] = stats[str(int(e["value"]))]
-    return result
-    
+def value_sum(values: dict) -> int:
+    return sum([v for k, v in values.items()])
 
-def calculate_stats(src, json_file, index) -> dict:
+
+def calc_value_percent(values: dict) -> dict:
+    result = {}
+    somme = value_sum(values)
+    for k, v in values.items():
+        result[k] = round(v * 100 / somme, 2)
+    return result
+
+
+def value_to_code(stats: dict, json_file, index: int) -> dict:
+    result = {}
+    for e in json_file["configurations"][index]["data"]["legend"]["details"]:
+        if str(float(e["value"])) in stats:
+            result[e["code"]] = stats[str(float(e["value"]))]
+    print(stats)
+    return result
+
+
+def calculate_stats(src, json_file, index):
     data = src.read(1)
     height, width = data.shape
     stats = {}
+    print(height, width)
     for row in range(height):
         for col in range(width):
             value = data[row, col]
             if str(value) in stats:
                 stats[str(value)] += 1
-            else :
+            else:
                 stats[str(value)] = 1
-    return(value_to_code(stats, json_file, index))
+    
+    return (value_to_code(stats, json_file, index))
 
-def cutter(raster, shapefile: str, savefile: str = "", json_file: str = "") -> list:
+
+def cutter(raster, shapefile: str, savefile: str = "", json_file: str = ""):
     tmp_file = "tmp_"
     output_dir = "../tmp"
     categorie_code = ""
@@ -42,10 +58,11 @@ def cutter(raster, shapefile: str, savefile: str = "", json_file: str = "") -> l
     index = 0
     out = {}
     out_with_code = {}
-    
+    print("ahah")
     raster_name = raster.split("\\")[-1]
-    for e in range(len(json_file["configurations"])) :
-        if "chromaColors" in json_file["configurations"][e]["data"]["legend"] :
+    for e in range(len(json_file["configurations"])):
+        if "chromaColors" in json_file["configurations"][e]["data"]["legend"]:
+            categorie_code = json_file['configurations'][e]["code"]
             return []
         if "path" in json_file['configurations'][e]["data"]:
             if json_file['configurations'][e]["data"]["path"].split("/")[-1] == raster_name:
@@ -80,29 +97,50 @@ def cutter(raster, shapefile: str, savefile: str = "", json_file: str = "") -> l
             print(
                 f"Raster créer avec succès et enregistré dans : {output_dir}/{tmp_file}{code}.tif ✔️")
         with rasterio.open(output) as dest:
-            out[code] = calculate_stats(dest, json_file, index)
-    print(out)
-    return out
+            out[code] = calc_value_percent(
+                calculate_stats(dest, json_file, index))
+    return ({"data": out, "code": categorie_code})
 
 
 def raster_cutter(raster, shapefile: str, savefile: str = "", json_file: str = ""):
-    
+
     json_output = {}
-    
-    with open(json_file, 'r', encoding='utf-8') as file :
+
+    with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
         if type(raster) == str:
             cutter(raster, shapefile, savefile, data)
         if type(raster) == list:
             for tif in raster:
-                if len(json_output) == 0 : json_output = cutter(tif, shapefile, savefile, data)
-                else :
-                    out = cutter(tif, shapefile, savefile, data)
-                    
-                    if len(out) == 0 : continue
-                    for key , value in out.items():
-                        json_output[key].append(value)
+
+                out = cutter(tif, shapefile, savefile, data)
                 
+                print(f"{tif}  -  {out}")
+                if len(out) == 0:
+                    continue
+                out_data = out["data"]
+                out_code = out["code"]
+                for key, value in out_data.items():
+                    if key in json_output:
+                        json_output[key].append(
+                            {"code": out_code, "data": value})
+                    else:
+                        json_output[key] = [{
+                            "code": out_code,
+                            "data": {
+                                "type": "static",
+                                "legend": {
+                                    "type": "singleValue",
+                                    "title": out_code.lower().replace("_", " "),
+                                    "details": [{"code" : e , "percentage" : i} for e , i in value.items()]
+                                    }}}]
+        if os.path.exists(savefile) == False:
+            os.makedirs(savefile)
+        for key, value in json_output.items() :
+            with open(f"{savefile}{key}.json", "w") as json_file:
+                json.dump({"configuration" : value},json_file)
+                print("Fichier de configuration créer avec susccès")
+            
 
 
 if __name__ == "__main__":
@@ -136,4 +174,4 @@ if __name__ == "__main__":
         print("Tout les fichiers on été supprimer ✔️")
         exit(0)
 
-    raster_cutter(raster, args.shapefile, json_file=args.json)
+    raster_cutter(raster, args.shapefile, args.save_folder, json_file=args.json)
